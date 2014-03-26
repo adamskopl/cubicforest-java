106856421b01a99733804a9a9a0e6f989aaa1ab7
askobodzinski <adam@skobo.pl>
2014-03-26 19:49:57 +0100

    #23 random commit

---
+++ cubicforest/src/org/adamsko/cubicforest/world/interactiveObjects/InteractiveObjectsMaster.java
@@ -0,0 +1,7 @@
+package org.adamsko.cubicforest.world.interactiveObjects;
+
+import org.adamsko.cubicforest.render.RenderableObjectsMaster;
+
+public interface InteractiveObjectsMaster extends RenderableObjectsMaster {
+
+}
