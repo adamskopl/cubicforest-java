da0965e6d59cdbeed1cf54bc3626a1ba72a41c0a
adamskopl <adam@skobo.pl>
2014-08-21 17:34:28 +0200

    WorldObjectVisitor

---
+++ cubicforest/src/org/adamsko/cubicforest/world/object/WorldObjectVisitor.java
@@ -0,0 +1,33 @@
+package org.adamsko.cubicforest.world.object;
+
+import org.adamsko.cubicforest.world.objectsMasters.entities.enemies.Enemy;
+import org.adamsko.cubicforest.world.objectsMasters.entities.heroes.Hero;
+import org.adamsko.cubicforest.world.objectsMasters.items.gatherCubes.GatherCube;
+import org.adamsko.cubicforest.world.objectsMasters.items.heroTools.tools.HeroToolOrange;
+import org.adamsko.cubicforest.world.objectsMasters.items.heroTools.tools.HeroToolTrap;
+
+/**
+ * 'Visitor' design pattern. 'With Default' variation assumption. <br>
+ * Interface for visitor classes visiting WorldObject classes. Every visitor
+ * classes family will be derived from default abstract visitor with all
+ * implementations, so not every visitor function will have to be implemented in
+ * concrete classes.
+ * 
+ * @author adamsko
+ * 
+ */
+public interface WorldObjectVisitor {
+
+	void visitWorldObject(WorldObject worldObject);
+
+	void visitHero(Hero hero);
+
+	void visitEnemy(Enemy enemy);
+
+	void visitGatherCube(GatherCube gatherCube);
+
+	void visitToolTrap(HeroToolTrap heroToolTrap);
+
+	void visitToolOrange(HeroToolOrange heroToolOrange);
+
+}
