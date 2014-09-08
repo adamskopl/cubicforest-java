826ec044a3952ccc4c8c47a556c8c6e4ab30d402
adamskopl <adam@skobo.pl>
2014-09-08 16:56:07 +0200

    adding gradle structure generated by gdx-setup.jar

---
+++ android/src/org/adamsko/cubicforest/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package org.adamsko.cubicforest.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import org.adamsko.cubicforest.Cubicforest;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Cubicforest(), config);
+	}
+}
---
+++ desktop/src/org/adamsko/cubicforest/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.adamsko.cubicforest.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.adamsko.cubicforest.Cubicforest;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Cubicforest(), config);
+	}
+}
