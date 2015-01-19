f24d3083ba670b82a79b3fdce4a0995aed824bf7
adamskopl <adam@skobo.pl>
2015-01-19 19:20:08 +0100

    adding PlayerUser

---
+++ core/src/org/adamsko/cubicforest/players/user/PlayerUser.java
@@ -0,0 +1,13 @@
+package org.adamsko.cubicforest.players.user;
+
+import org.adamsko.cubicforest.players.Player;
+
+/**
+ * Player: user of the application. Issuing decisions by input.
+ * 
+ * @author adamsko
+ * 
+ */
+public class PlayerUser implements Player {
+
+}
