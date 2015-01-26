25f217b67c71a9034332dbfb895d3ee125f379c8
adamskopl <adam@skobo.pl>
2015-01-26 17:07:40 +0100

    players/decisionOrdersReplay/PlayerDecisionOrdersReplay.java

---
+++ core/src/org/adamsko/cubicforest/players/decisionOrdersReplay/PlayerDecisionOrdersReplay.java
@@ -0,0 +1,7 @@
+package org.adamsko.cubicforest.players.decisionOrdersReplay;
+
+import org.adamsko.cubicforest.players.PlayerBase;
+
+public class PlayerDecisionOrdersReplay extends PlayerBase {
+
+}
